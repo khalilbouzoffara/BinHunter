9761c9809ad37ec0df3763a535d16b13a0522cdf
khalilbouzoffara <khalil.bouzoffara@ensi-uma.tn>
2024-10-27 12:30:14 +0100

    fix hover

--- front-end/src/components/Dashboard/TableRow.jsx
+++ front-end/src/components/Dashboard/TableRow.jsx
@@ -1,16 +1,31 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const TableRow = ({ data, columns }) => (
-  <tr>
-    {columns.map((item, index) => (
-      <td
-        key={index}
-        className="px-4 py-3 text-white text-sm font-light leading-normal"
-      >
-        {item.id === 'hash' ? data[item.id]?.slice(0, 10) : data[item.id] ?? 'N/A'}
-      </td>
-    ))}
-  </tr>
-);
+const TableRow = ({ data, columns }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    // Assuming each row has a unique ID for routing
+    navigate(`/item/${data.id}`);
+  };
+
+  return (
+    <tr
+      onClick={handleClick}
+      className="cursor-pointer hover:bg-gray-800"
+    >
+      {columns.map((item, index) => (
+        <td
+          key={index}
+          className="px-4 py-3 text-white text-sm font-light leading-normal"
+        >
+          {item.id === "hash"
+            ? data[item.id]?.slice(0, 10)
+            : data[item.id] ?? "N/A"}
+        </td>
+      ))}
+    </tr>
+  );
+};
 
 export default TableRow;
